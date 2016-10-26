0d3356c72f6ef275c0fdbccdce0af2140dc72cff
Bob Breznak <bob.breznak@gmail.com>
2016-10-26 15:39:31 -0400

    Fix linter issue

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ co(function *() {
     bucket: argv.bucket,
     region: argv.r || argv.region || 'us-east-1',
     cwd: argv.cwd || '',
-	  profile: argv.profile,    
+    profile: argv.profile,
     gzip: (argv.gzip ? 'gzip' : undefined),
   };
 
