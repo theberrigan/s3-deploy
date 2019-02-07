ef6f7674ff4529e34ec1e90d88bd79c41adeed39
Max Winderbaum <maxwinderbaum@gmail.com>
2019-02-07 00:22:02 -0800

    Move noCache to options, restrict to one caching arg
    Also add some tests to verify functionality

--- test/unit/index.js
+++ test/unit/index.js
@@ -12,5 +12,29 @@ describe('#parseCliArgsToOptions()', () => {
     it('should be array if arg is given', () => {
       expect(parseCliArgsToOptions([0, 0, '--gzip', 'js,css,html']).gzip).to.deep.equal(['js', 'css', 'html']);
     });
+
+    it('should accept a --cache argument to set the max-age Cache-Control header', () => {
+      const argv = [0, 0, '--cache', 64];
+      const parsedOptions = parseCliArgsToOptions(argv);
+      expect(parsedOptions.cacheControl).to.equal('max-age=64');
+    });
+
+    it('should accept a --noCache argument to disable caching in the Cache-Control header', () => {
+      const argv = [0, 0, '--noCache', '--otherParam'];
+      const parsedOptions = parseCliArgsToOptions(argv);
+      expect(parsedOptions.cacheControl).to.equal('no-cache, no-store, must-revalidate');
+    });
+
+    it('should accept an --immutable argument to set the immutable Cache-Control header', () => {
+      const argv = [0, 0, '--otherParam', '--immutable'];
+      const parsedOptions = parseCliArgsToOptions(argv);
+      expect(parsedOptions.cacheControl).to.equal('immutable');
+    });
+
+    it('should not accept more than one caching argument', () => {
+      const argv = [0, 0, '--noCache', '--cache', 34];
+      const parsedOptions = parseCliArgsToOptions(argv);
+      expect(parsedOptions.cacheControl).to.equal('no-cache, no-store, must-revalidate');
+    });
   });
 });
